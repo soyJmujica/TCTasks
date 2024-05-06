81f0084c1773087dabed6d37ef74b8dc8b78969a
Jose Mujica <master1495@gmail.com>
2024-05-06 19:23:10 -0400

    Actualizacion de settings

--- TestEmail/settings.py
+++ TestEmail/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-685wqem17jpnbi=#8l=q%#-_tk(jmzxdge(5q&a*1)!47su1w2"
+SECRET_KEY = "SECRET_KEY"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
@@ -170,10 +170,10 @@ LOGIN_URL = "/yodjango.core.mail.backends.smtp.EmailBackenduarenotlogged/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+'''EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST ='smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER ='master1495@gmail.com'
 EMAIL_HOST_PASSWORD ='uoloruuisndpenad'
 EMAIL_USE_TLS = True 
-EMAIL_USE_SSL = False
+EMAIL_USE_SSL = False'''
