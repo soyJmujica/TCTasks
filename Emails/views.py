2d3d29486533c2c4b08d7d8227d29829dee80e6d
Jose Mujica <master1495@gmail.com>
2024-05-07 17:10:26 -0400

    Prueba de base de datos sqlite3

--- Emails/views.py
+++ Emails/views.py
@@ -60,6 +60,7 @@ def	signin(request):
     return render(request, 'login.html', {"form":form, "encabezado":'Log In'})
         
 def ingreso(request):
+    login = AuthenticationForm()
     return render(request, 'ingreso.html', {'login':login, "encabezado": "Sign Up  |  Log In"})
 
 def not_logged(request):
